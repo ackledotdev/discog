57462d0419e9b897a5f45c6a572fb0a09124a095
Akhil Pillai <ackledotdev@gmail.com>
2025-07-14 18:03:37 -0700

    upgrade deps

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -5,6 +5,7 @@ import {
 	Message,
 	channelMention,
 	codeBlock,
+	time,
 	userMention
 } from 'discord.js';
 import { getGuildAuditLoggingChannel } from './a.getGuildConf';
@@ -15,92 +16,98 @@ import { DatabaseKeys, DENO_KV_URL } from '../config';
 import { openKv } from '@deno/kv';
 import { Snowflake, TimestampStyles } from 'discord.js';
 
-export const execute = async (old?: Message, updated: Message) => {
+export const execute = async (oldMessage: Message, newMessage: Message) => {
 	try {
-		if (!old) throw new Error('Gracefully caught ReferenceError: old is not defined');
 		if (
-			old.author.bot ||
-			!old.inGuild() ||
-			updated.author.bot ||
-			!updated.inGuild() ||
-			old.content === updated.content // Discord seems to send an update event for every embed update, so we need to filter out those
+			oldMessage.author.bot ||
+			!oldMessage.inGuild() ||
+			newMessage.author.bot ||
+			!newMessage.inGuild() ||
+			oldMessage.content === newMessage.content // Discord seems to send an update event for every embed update, so we need to filter out those
 		)
 			return;
 		await (
-			await getGuildAuditLoggingChannel(updated.guild)
+			await getGuildAuditLoggingChannel(newMessage.guild)
 		)?.send({
 			embeds: [
 				new EmbedBuilder()
 					.setTitle('Message Updated')
-					.setDescription(updated.url)
+					.setDescription(newMessage.url)
 					.setFields(
 						{
 							name: 'Author',
-							value: userMention(updated.author.id)
+							value: userMention(newMessage.author.id)
 						},
 						{
 							name: 'Channel',
-							value: channelMention(updated.channel.id)
+							value: channelMention(newMessage.channel.id)
 						},
 						{
 							name: 'Message ID',
-							value: updated.id
+							value: newMessage.id
 						},
 						{
 							name: 'Initial Content',
-							value: old.content
+							value: oldMessage.content
 						},
 						{
 							name: 'Updated Content',
-							value: updated.content
+							value: newMessage.content
 						}
 					)
 					.setColor(0x0000ff)
 					.setTimestamp()
 					.setFooter({
-						iconURL: updated.guild.members.me?.displayAvatarURL(),
+						iconURL: newMessage.guild.members.me?.displayAvatarURL(),
 						text: 'Powered by DisCog'
 					})
 			]
 		});
 	} catch (e) {
-		await sendError(updated.client, e);
+		await sendError(newMessage.client, e as Error);
+	}
+
+	async function sendError(client: Client, e: Error) {
+		for (const devId of (
+			await (await openKv(DENO_KV_URL)).get<Snowflake[]>([DatabaseKeys.Devs])
+		)?.value ?? []) {
+			client.users.fetch(devId).then(user => {
+				const date = new Date();
+				user.send({
+					embeds: [
+						new EmbedBuilder()
+							.setTitle('Error Log: Message Update Event')
+							.setDescription(e.message)
+							.addFields({
+								name: 'Stack Trace',
+								value: codeBlock(e.stack ?? '')
+							})
+							.addFields({
+								name: 'Old Message',
+								value: codeBlock(
+									oldMessage
+										? JSON.stringify(oldMessage, undefined, 2)
+										: 'undefined'
+								)
+							})
+							.addFields({
+								name: 'New Message',
+								value: codeBlock(JSON.stringify(newMessage, undefined, 2))
+							})
+							.addFields({
+								name: 'ISO 8601 Timestamp',
+								value: date.toISOString()
+							})
+							.addFields({
+								name: 'Localized DateTime',
+								value: time(date, TimestampStyles.LongDateTime)
+							})
+							.setColor(0xff00ff)
+							.setTimestamp()
+					]
+				});
+			});
+		}
+		console.error(e);
 	}
 };
-
-async function sendError(client: Client, e: Error) {
-	for (const devId of (
-		await (await openKv(DENO_KV_URL)).get<Snowflake[]>([DatabaseKeys.Devs])
-	)?.value ?? []) {
-		client.users.fetch(devId).then(user => {
-			const date = new Date();
-			user.send({
-				embeds: [
-					new EmbedBuilder()
-						.setTitle('Error Log: Message Update Event')
-						.setDescription(e.message)
-						.addFields({ name: 'Stack Trace', value: codeBlock(e.stack ?? '') })
-						.addFields({
-							name: 'Old Message',
-							value: codeBlock(old ? JSON.stringify(old, undefined, 2) : 'undefined')
-						})
-						.addFields({
-							name: 'New Message',
-							value: codeBlock(JSON.stringify(updated, undefined, 2))
-						})
-						.addFields({
-							name: 'ISO 8601 Timestamp',
-							value: date.toISOString()
-						})
-						.addFields({
-							name: 'Localized DateTime',
-							value: time(date, TimestampStyles.LongDateTime)
-						})
-						.setColor(0xff00ff)
-						.setTimestamp()
-				]
-			});
-		});
-	}
-	console.error(e);
-}
